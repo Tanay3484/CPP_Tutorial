25b26f0cecf7a6d047db6c4971b7f68893028ffa
Tanay3484 <tanay8705@gmail.com>
2022-07-31 20:22:10 +0530

    Implementation of Stacks using Arrays

--- Data Structures/Stack/stacksImplementationUsingArray.cpp
+++ Data Structures/Stack/stacksImplementationUsingArray.cpp
@@ -1,71 +1,80 @@
 #include <iostream>
 using namespace std;
 
-#define n 100 //Dummy value for n
+#define n 100 // Dummy value for n
 
-class stack{
-    private:
-    int* arr;
+class stack
+{
+private:
+    int *arr;
     int top;
 
-    public:
-    stack(){
+public:
+    stack()
+    {
         arr = new int[n];
         top = -1;
     }
 
-    void push(int x){
-        if(top==n-1){
-            cout<<"Stack Overflow"<<endl;
-            return ;
+    void push(int x)
+    {
+        if (top == n - 1)
+        {
+            cout << "Stack Overflow" << endl;
+            return;
         }
         top++;
         arr[top] = x;
     }
 
-    void pop(){
-        if(top==-1){
-            cout<<"Stack Empty"<<endl;
-            return ;
+    void pop()
+    {
+        if (top == -1)
+        {
+            cout << "Stack Empty" << endl;
+            return;
         }
-        cout<<"Popped element : "<<arr[top]<<endl;
+        cout << "Popped element : " << arr[top] << endl;
         top--;
     }
 
-    int Top(){
-        if(top==-1){
-            cout<<"Stack empty"<<endl;
+    int Top()
+    {
+        if (top == -1)
+        {
+            cout << "Stack empty" << endl;
             return -1;
         }
         return arr[top];
     }
 
-    bool empty(){
-        return top==-1;
+    bool empty()
+    {
+        return top == -1;
     }
 };
 
 int main()
 {
     int size;
-    cout<<"Enter the size of the stack : ";
-    cin>>size;
-    cout<<endl;
-    #undef n
-    #define n size
+    cout << "Enter the size of the stack : ";
+    cin >> size;
+    cout << endl;
+#undef n
+#define n size
     stack st;
     st.push(1);
     st.push(2);
     st.push(3);
-    cout<<"Top element : "<<st.Top()<<endl;
+    cout << "Top element : " << st.Top() << endl;
     st.pop();
-    cout<<"Top element : "<<st.Top()<<endl;
+    cout << "Top element : " << st.Top() << endl;
     st.pop();
-    cout<<"Top element : "<<st.Top()<<endl;
+    cout << "Top element : " << st.Top() << endl;
     st.pop();
-    cout<<"Top element : "<<st.Top()<<endl;
+    cout << "Top element : " << st.Top() << endl;
     st.pop();
     st.pop();
-    cout<<(st.empty()?"Stack is empty":"Stack is not empty")<<endl;
+    cout << (st.empty() ? "Stack is empty" : "Stack is not empty") << endl;
     return 0;
 }
